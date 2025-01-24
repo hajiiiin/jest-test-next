c6749fea6497f41ec4aa5323b2f983b02e9a69ed
hajiiiin <101108440+hajiiiin@users.noreply.github.com>
2025-01-24 17:52:15 +0900

    build

--- with-jest-app/jest.config.js
+++ with-jest-app/jest.config.js
@@ -9,6 +9,11 @@ const createJestConfig = nextJest({
 const customJestConfig = {
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   testEnvironment: "jsdom",
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+.(ts|tsx|js|jsx)$": "babel-jest",
+  },
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
